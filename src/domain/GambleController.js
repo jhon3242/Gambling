6e93ba9198a3a8227816bf26d4000ff7982eb696
jhon3242 <rldotnr2@gmail.com>
2023-02-01 15:50:33 +0900

    feat : 배팅판 구현

--- src/domain/GambleController.js
+++ src/domain/GambleController.js
@@ -8,34 +8,56 @@ window.onload = function() {
 	initWheel();
 	let user = new User("choi", 10000);
 	contorller = new GambleController(user);
+	document.getElementById("asset").innerText =contorller.getAsset();
 }
 
 function afterRolling() {
-	contorller.start(1, 1000);
+	let result = getRollingResult();
+	let total = 0;
+	alert(`룰렛 결과 : ${result}번 입니다.\n`);
+	total += contorller.start(MULTIPLE.x1, document.getElementById("x1B").value, result);
+	total += contorller.start(MULTIPLE.x3, document.getElementById("x3B").value, result);
+	total += contorller.start(MULTIPLE.x5, document.getElementById("x5B").value, result);
+	total += contorller.start(MULTIPLE.x10, document.getElementById("x10B").value, result);
+	total += contorller.start(MULTIPLE.x20, document.getElementById("x20B").value, result);
+	alert(`배팅 결과 : ${total >= 0 ? `+${total}` : total}`);
+	document.getElementById("asset").innerText =contorller.getAsset();
+}
+
+function resetRolling() {
+	theWheel.rotationAngle=0; 
+	theWheel.draw();
+	document.getElementById("bigButton").disabled = false;
+	document.getElementById("x1B").value = null;
+	document.getElementById("x3B").value = null;
+	document.getElementById("x5B").value = null;
+	document.getElementById("x10B").value = null;
+	document.getElementById("x20B").value = null;
 }
 
 class GambleController {
 	
 	constructor(user) {this._user = user};
 
-	start(pick, money) {
+	start(pick, money, result) {
+		if (money === "")
+			return 0;
+		money = +money;
 		try {
 			this._user.withdraw(money);
 		} catch (err){
 			alert(err);
 			return ;
 		}
-		
-		const result = getRollingResult();
-		let msg = `룰렛 결과 : ${result}번 입니다.\n`;
-		
-		if (pick === result) {
+		result = getMutipleByKey("x" + result);
+		if (pick.multiple == result) {
 			this._user.deposit(money * result);
-			msg += "맞췄습니다!"
-			alert(msg);
-			return ;
+			return money * result;
 		}
-		msg += "틀렸습니다.!"
-		alert(msg);
+		return -money;
+	}
+
+	getAsset() {
+		return this._user.getAsset();
 	}
 }
--- src/domain/Multiple.js
+++ src/domain/Multiple.js
@@ -1,42 +1,12 @@
 "use strict"
 const MULTIPLE = {
-	x1 : {mutiple : 2, count : 12, color : "#efe61f"},
-	x3 : {mutiple : 3, count : 5, color : "#f7a416"},
-	x5 : {mutiple : 5, count : 4, color : "#3f297e"},
-	x10 : {mutiple : 10, count : 2, color : "#be107f"},
-	x20 : {mutiple : 20, count : 1, color : "#e7167b"},
+	x1 : {multiple : 2, count : 12, color : "#efe61f"},
+	x3 : {multiple : 3, count : 5, color : "#f7a416"},
+	x5 : {multiple : 5, count : 4, color : "#3f297e"},
+	x10 : {multiple : 10, count : 2, color : "#be107f"},
+	x20 : {multiple : 20, count : 1, color : "#e7167b"},
 }
 
-function getProducts(obj) {
-	return Object.keys(obj).map(key => {
-		let tmp = [];
-		for (let i=0; i<MULTIPLE[key].count; i++) {
-			tmp.push(key);
-		}
-		return tmp;
-	}).flatMap(val => val);
+function getMutipleByKey(key) {
+	return MULTIPLE[key].multiple;
 }
-
-function shuffle(array) {
-	array.sort(() => Math.random() - 0.5);
-}
-
-function getColors(products){
-	let result = [];
-	for (let val of products){
-		result.push(MULTIPLE[val].color);
-	}
-	return result;
-}
-
-function getRoulettes(obj) {
-	let products = getProducts(obj);
-	shuffle(products);
-	let colors = getColors(products);
-	return [products, colors];
-}
-
-function getColor(obj, mutiple) {
-	return obj[mutiple]["color"];
-}
-
